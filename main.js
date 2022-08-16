fd184a43465d9a2ee0790fdebfc40f99fbc80881
Shehabmohamed86 <shehabmohamed8688@gmail.com>
2022-08-16 19:41:25 -0400

    datareqest

--- main.js
+++ main.js
@@ -7,3 +7,22 @@ axios.get("https://swapi.dev/api/people/?page=2")
   console.log(err)
   console.log('its not working')
 })
+
+
+
+function changeBodyColor(color,deley){
+ return new Promise((resolve,reject)=>{
+  setTimeout(()=>{
+    document.body.style.backgroundColor = color
+    resolve()
+  },deley)
+ });
+};
+
+async function lightShow (){
+  await changeBodyColor('orange',1000);
+  await changeBodyColor('red',1000);
+  await changeBodyColor('yellow',1000);
+  await changeBodyColor('blue',1000);
+}
+lightShow()
